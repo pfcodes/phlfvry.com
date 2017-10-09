758f4f52730e389cd7b8a802a9d4088dcc5413a8
Phil Fevry <pf@phlfvry.com>
2017-10-08 22:16:54 -0400

    added bootstrap grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var nodemon = require('nodemon')
+var csso = require('gulp-csso')
 
 gulp.task('default', ['sass'])
 
 gulp.task('sass', function() {
 	return gulp.src('sass/style.sass')
 	.pipe(sass())
+	.pipe(csso())
 	.pipe(gulp.dest('public/stylesheets'))
 })
